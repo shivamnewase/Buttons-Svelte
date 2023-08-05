e74b08e20da5cc08ac731dec4fc20a8adcbebb18
Shivam Newase <shivam.newase@inflectionzone.com>
2023-08-05 15:27:02 +0530

    delete old code

--- src/lib/index.js
+++
@@ -1,5 +0,0 @@
-// src/lib/index.js
-import Button from './PrimaryButton.svelte';
-
-export { Button };
-// export { label ,customClass};
--- src/routes/+page.svelte
+++
@@ -1,8 +0,0 @@
-<!-- <script lang="ts">
-    import Button from '../lib/index.js'
-    </script>
-
-
-<Button/> -->
-
-<!-- <h1>Hello</h1> -->
